eb226f0608a2d5c0ace00d92b5811cd552decc7b
Kelly Ferrone <kelly@duplocloud.net>
2024-07-10 10:31:45 -0600

    refactored the update_image

--- src/scripts/update_image.sh
+++ src/scripts/update_image.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 #Param substitution
-SERVICE=$(circleci env subst "${SERVICE}")
-DUPLO_TENANT=$(circleci env subst "${DUPLO_TENANT}")
-SERVICE_IMAGE=$(circleci env subst "${SERVICE_IMAGE}")
+export DUPLO_TENANT=$(circleci env subst "${PARAM_TENANT}")
+PARAM_TYPE=$(circleci env subst "${PARAM_TYPE}")
+PARAM_NAME=$(circleci env subst "${PARAM_NAME}")
+PARAM_IMAGE=$(circleci env subst "${PARAM_IMAGE}")
+PARAM_WAIT=$(circleci env subst "${PARAM_WAIT}")
 
-echo "Tenant is: ${DUPLO_TENANT}"
-echo "Service Name: ${SERVICE}"
-echo "Service Image: ${SERVICE_IMAGE}"
+echo "Updating ${DUPLO_TENANT}/${PARAM_NAME} on ${DUPLO_HOST}"
 
-duploctl service update_image "$SERVICE" "$SERVICE_IMAGE"
+# check if we should wait
+DUPLO_WAIT=""
+if [ "$PARAM_WAIT" = "true" ]; then
+  DUPLO_WAIT="--wait"
+fi
+
+duploctl "$PARAM_TYPE" update_image "$PARAM_NAME" "$PARAM_IMAGE" "$DUPLO_WAIT"
